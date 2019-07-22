0acc39b389be445af9139e06aa19e805a93d714c
Artluevil <artluevil139@gmail.com>
2019-07-22 17:15:47 +0200

    fix bug

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import Header from './components/Header'
 import DataFilms from './components/DataFilms'
 import DataPosters from './components/DataPosters'
 import DataEvents from './components/DataEvents'
+import CinemaScreen from './components/CinemaScreen'
 import ItemOverview from './components/ItemOverview'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-
-const CinemaScreen = require('./components/CinemaScreen')
 
 export default class App extends Component {
   state = {
@@ -19,7 +18,7 @@ export default class App extends Component {
       <Router>
         <div>
           <Header />
-          <Route exact path='/kino-website/' render={(routeProps) => (<CinemaScreen {...routeProps} events={this.state.events} posters={this.state.posters} films={this.state.films} />)}/>
+          <Route exact path='/' render={(routeProps) => (<CinemaScreen {...routeProps} events={this.state.events} posters={this.state.posters} films={this.state.films} />)}/>
           <Route exact path='/PosterOverview' component={ItemOverview} />
         </div>
       </Router>
